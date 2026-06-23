c6b6e1c3f33f06e8bd5f58783072c3ffc3ef3a6b
Luciano Saavedra <mattensohn64@gmail.com>
2026-06-23 09:29:36 -0400

    Se añade contraseña de redis

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -9,6 +9,7 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
     this.client = new Redis({
       host: process.env.REDIS_HOST || '127.0.0.1',
       port: parseInt(process.env.REDIS_PORT || '6379'),
+      password: process.env.REDIS_PASSWORD,
     });
   }
 
